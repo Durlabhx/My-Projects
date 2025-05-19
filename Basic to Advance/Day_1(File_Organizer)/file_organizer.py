6dc80c2ee2db2a32459962b244649221583754f7
Durlabhx <Durlabhkashyaptech@gmail.com>
2025-05-19 09:36:27 +0530

    feature-Undo_last_moved

--- Basic to Advance/Day_1(File_Organizer)/file_organizer.py
+++ Basic to Advance/Day_1(File_Organizer)/file_organizer.py
@@ -43,4 +43,39 @@ for file_name in os.listdir(folder_path):
         with open(log_file, 'a', encoding="utf-8")as log:
             log.write(f"{file_name} -> {folder if moved else 'Others'} at {datetime.now()}\n")
 
+def undo_last_move(folder_path):
+    log_file=os.path.join(folder_path, 'log.txt')
 
+    if not os.path.exists(log_file):
+        print("No log file found. Cannot Undo.")
+        return
+    
+    with open(log_file, 'r', encoding='utf-8') as f:
+        lines = f.readlines()
+
+    if not lines:
+        print("Log is empty. Nothing to undo.")
+        return 
+    last_line = lines[-1]
+    try:
+        file_name=last_line.split("->")[0].strip()
+        folder=last_line.split("->")[1].split('at')[0].strip()
+        source=os.path.join(folder_path, folder, file_name)
+        destination=os.path.join(folder_path, file_name)
+
+        if os.path.exists(source):
+            shutil.move(source, destination)
+            print(f"MOved back: {file_name} from {folder}/ to main folder")
+
+            #Remove last line from log
+            with open(log_file, 'w', encoding='utf-8') as f:
+                f.writelines(lines[:-1])
+        else:
+            print("File not found in expected location")
+
+    except Exception as e:
+        print("Error while undoing:", e)
+
+undo = input("Do you want to undo last moved file (yes/no): ").lower()
+if undo == "yes":
+    undo_last_move(folder_path)
