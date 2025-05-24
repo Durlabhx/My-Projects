1cd9a76e3bd81bebf25ad6694c1f77281c7aed05
Durlabhx <Durlabhkashyaptech@gmail.com>
2025-05-24 11:11:06 +0530

    feat: Delete Passwords

--- Python/Password-Manager/main.py
+++ Python/Password-Manager/main.py
@@ -35,25 +35,45 @@ def view_passwords():
         print(f"\n🌐 {website}")
         print(f"   👤 Username: {creds['username']}")
         print(f"   🔑 Password: {creds['password']}")
+    print(f"[i] Total saved passwords: {len(data)}")
+    return data
+
+def delete_passwords():
+    data = view_passwords()
+    delete_input = input("Enter Website name to delete: ").strip().title()
+    if delete_input in data:
+        confirm = input(f"Are you sure you want to delete {delete_input}? (y/n): ").strip().lower()
+        if confirm == 'y':
+            del data[delete_input]
+            save_data(data)
+            print(f"[X] Password for '{delete_input}' deleted.")
+        else:
+            print('[i] Deletion cancelled.')
+    else:
+        print(f"[!] Website : {delete_input} not found")
 
 def menu():
     while True:
         print("\n===== PASSWORD MANAGER =====")
         print("1. Add New Password")
         print("2. View Saved Passwords")
-        print("3. Exit")
-        choice = input("Enter choice (1/2/3): ")
+        print("3. Delete Saved Passwords")
+        print("4. Exit")
+        choice = input("Enter choice (1/2/3/4): ")
 
         if choice == "1":
-            website = input("Website: ")
-            username = input("Username: ")
-            password = input("Password: ")
+            website = input("Website: ").strip()
+            username = input("Username: ").strip()
+            password = input("Password: ").strip()
             add_password(website.title(), username, password)
 
         elif choice == "2":
             view_passwords()
 
         elif choice == "3":
+            delete_passwords()
+
+        elif choice == "4":
             print("Goodbye!")
             break
 
